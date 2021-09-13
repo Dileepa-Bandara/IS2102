5ac4f4e61df0ef3969e7a661510225fa0dbbf4b4
Dileepa Bandara <dileepa2018al1997@gmail.com>
2021-09-13 11:35:03 +0530

    first commit by Dileepa

---
+++ interfaces/employee/index.php
@@ -0,0 +1,134 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="./interfaces/employee/index.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="top-container" id="top-container">
+
+        <div class="menu__icon" id="menu__icon"><button }><img src="./assets/menu.png" alt=""> </button></div>
+        <div class="title">HRM System</div>
+        <div class="position">Employee</div>
+        <div class="notification"><a href="#"><img src="./assets/notification.png" alt="Notification"></a></div>
+        <div class="logout"><button>Logout</button></div>
+    </div>
+
+
+    <div class="side-container" id="side-container">
+
+        <div class="avatar"><img src="./assets/avatar.png" alt="Avatar"></div>
+        <div class="name">Name AAA</div>
+        <div class="buttons">
+            <button class="dropButton"><a href="index.php?Page=dashboard.php">Dashoboard</a></button>
+            <button class="dropButton"> Leaves</button>
+            <div class="drop-content">
+                <a href="index.php?Page=applyLeave.php">Apply Leaves</a>
+                <a href="#">Link 2</a>
+                <a href="#">Link 3</a>
+            </div>
+
+        </div>
+    </div>
+
+    <div class="content" id="content">
+
+        <?php
+
+        if (!isset($_GET["Page"])) {
+            include("pages" . "/" . "dashboard.php");
+        }
+
+        if (!empty($_GET["Page"])) {
+            $pageName = $_GET["Page"];
+            echo "<h2>page name is $pageName</h2>";
+            $pages_directory = "pages";
+            echo "<h2>Directory name is $pages_directory</h2>";
+
+
+            //get page directory
+            $pages_folder = scandir($pages_directory, 1);
+            echo "<pre>";
+            print_r($pages_folder);
+            echo "</pre>";
+            //remove unnecessary files
+            unset($pages_folder[1], $pages_folder[3], $pages_folder[4]);
+            echo "<pre>";
+            print_r($pages_folder);
+            echo "</pre>";
+
+
+
+
+            if (in_array($pageName, $pages_folder)) {
+
+                include($pages_directory . "/" . $pageName);
+            } else if (!isset($_GET["Page"]) || !empty($_GET["Page"]) == "dashboard") {
+                include($pages_directory . "/" . "dashboard.php");
+            } else {
+                echo "</h2>OOPS....NOT FOUND </h2>";
+            }
+        }
+
+
+
+
+
+
+
+        ?>
+    </div>
+
+    <script>
+        //we should stop page refreshing after button click
+        var menuButton = document.getElementById("menu__icon");
+        var width = document.getElementById("side-container");
+        var contentMargin = document.getElementById("content");
+        var dropButton = document.getElementsByClassName("dropButton")
+        var topContainer = document.getElementById("top-container");
+        menuButton.addEventListener("click", function() {
+
+
+            if (width.style.width === "12em") {
+                width.style.width = 0;
+                width.style.padding = 0;
+                contentMargin.style.marginLeft = "0em";
+
+                width.style.opacity = 0;
+
+            } else {
+                width.style.width = "12em";
+                contentMargin.style.marginLeft = "12em";
+                width.style.opacity = 10;
+                width.style.padding = "1em"
+                topContainer.style.width = "100%";
+
+
+            }
+        });
+
+
+        var i = 0;
+        for (i = 0; i < dropButton.length; i++) {
+            dropButton[i].addEventListener("click", function(e) {
+
+                var dropdownContent = this.nextElementSibling;
+                if (dropdownContent.style.display === "block") {
+                    dropdownContent.style.display = "none";
+                    prevent.default();
+                } else {
+
+                    dropdownContent.style.display = "block";
+
+                }
+            });
+        }
+    </script>
+</body>
+
+</html>
---
+++ pages/dashboard.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="./pages/css/dashboard.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="dashboard">
+
+        <h2 style="color: red ">Dashboard Page</h2>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. A facilis vero facere harum sit tempore dolore quos nihil. Eos aut eum velit dolorum sed natus blanditiis quos ipsa enim, quibusdam beatae. Voluptates exercitationem totam rerum deleniti corrupti itaque dicta consectetur labore esse obcaecati harum ducimus fugit qui sed, doloribus porro ipsam, mollitia maiores necessitatibus est. Magnam ex voluptates ratione sint iure totam suscipit dolorem praesentium distinctio, tenetur delectus eveniet optio cupiditate, laboriosam qui iste unde odit, repellendus aut minus modi temporibus tempora pariatur! Facere eum, labore minus autem delectus iusto impedit tempora vero repudiandae officiis quibusdam laborum deleniti, ut cumque qui fugit cupiditate esse molestias id culpa repellendus. Autem at facere molestias non sint assumenda nam ad enim optio adipisci, aspernatur incidunt quod perspiciatis totam omnis facilis dicta, est et obcaecati doloribus ducimus repudiandae mollitia cupiditate vel? Iure neque aperiam voluptatibus repellendus labore laborum veniam dolorum rerum, nisi repellat, quas commodi recusandae ea earum natus mollitia in officia consequuntur! Nobis obcaecati sit assumenda illum, atque quae nemo nisi corrupti incidunt sunt itaque aperiam mollitia illo id repudiandae commodi modi ex accusantium! Incidunt nam deleniti nesciunt quam labore, minima, ducimus laudantium voluptatum ea voluptate quos. Aut aspernatur blanditiis non omnis necessitatibus.</p>
+
+    </div>
+
+</body>
+
+</html>
